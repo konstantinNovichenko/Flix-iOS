446026bf3a0e06ea498192e42479893276af3769
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-09-20 18:05:28 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Konstantin Novichenko on 9/20/20.
+//  Copyright © 2020 konst_nvc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
